be72d6e0cd4e8a56f73a275e4d4ac024c1ae5594
kanfet <byte.developer@gmail.com>
2013-11-29 17:23:04 +0600

    products without variants for alytics

--- lib/export/alytics_exporter.rb
+++ lib/export/alytics_exporter.rb
@@ -1,7 +1,44 @@
+# encoding: utf-8
+
 module Export
   class AlyticsExporter < YandexMarketExporter
     def initialize
       @utms = '?utm_source=alytics&utm_medium=alytics&utm_campaign=alytics'
+    end
+
+    protected
+
+    def offer_vendor_model(xml, product)
+      images = product.images.limit(10)
+      gender = case product.gender
+                 when 1 then 'Мужской'
+                 when 2 then 'Женский'
+                 else ''
+               end
+
+      xml.offer(type: 'vendor.model', available: true, id: product.id) do
+        xml.url "http://#{@host}/id/#{product.id}#{@utms}"
+        xml.price product.price
+        xml.currencyId currency_id
+        xml.categoryId product_category_id(product)
+        xml.market_category market_category(product)
+        images.each do |image|
+          xml.picture image_url(image)
+        end
+        xml.delivery true
+        xml.vendor product.brand.name if product.brand
+        if add_alt_vendor? && product.brand && product.brand.alt_displayed_name.present?
+          xml.vendorAlt product.brand.alt_displayed_name
+        end
+        xml.vendorCode product.sku
+        xml.model model_name(product)
+        xml.description product_description(product) if product_description(product)
+        xml.country_of_origin product.country.name if product.country
+        xml.param product.colour, :name => 'Цвет'
+        xml.param gender, :name => 'Пол' if gender.present?
+        xml.param product.localized_age, :name => 'Возраст' if product.age
+        xml.param product.picture_type, :name => 'Тип рисунка' if product.picture_type
+      end
     end
 
     def preferred_category
