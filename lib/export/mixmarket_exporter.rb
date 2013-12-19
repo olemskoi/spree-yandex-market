e6105b666c79bde179096b28303866cf443b7209
kanfet <byte.developer@gmail.com>
2013-12-20 02:48:42 +0600

    <name> format for "mixmarket"

--- lib/export/mixmarket_exporter.rb
+++ lib/export/mixmarket_exporter.rb
@@ -3,5 +3,19 @@ module Export
     def initialize
       @utms = '?utm_source=mixmarket&utm_medium=mixmarket&utm_campaign=mixmarket'
     end
+
+    protected
+
+    def offer_vendor_model(xml, product)
+      xml.offer(id: product.id) do
+        xml.url "http://#{@host}/id/#{product.id}#{@utms}"
+        xml.price product_price(product)
+        xml.currencyId currency_id
+        xml.categoryId product_category_id(product)
+        xml.picture image_url(product.images.first) if product.images.first.present?
+        xml.name model_name(product)
+        xml.description product_description(product) if product_description(product)
+      end
+    end
   end
 end
