22eef7e42df68f9679e90736cf9f275e21b3c20e
kanfet <byte.developer@gmail.com>
2014-06-02 15:35:28 +0600

    'oldprice' removed for yandex_market exporter

--- lib/export/yandex_market_exporter.rb
+++ lib/export/yandex_market_exporter.rb
@@ -185,7 +185,7 @@ module Export
     end
 
     def additional_params_for_offer(xml, product, variant)
-      xml.oldprice variant.old_price if variant.old_price.to_i > 0
+      # nothing
     end
 
     def namespaces
