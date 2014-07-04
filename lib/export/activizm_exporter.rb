22052d1d7f69650ed57a0e588a894424e272f89f
Alexey Gordienko <alx@anadyr.org>
2014-07-05 00:56:23 +1200

    Refactoring Activizm Exporter

--- lib/export/activizm_exporter.rb
+++ lib/export/activizm_exporter.rb
@@ -30,31 +30,67 @@ module Export
           xml.picture image_url(image)
         end
         xml.delivery true
-        xml.local_delivery_cost @config.preferred_local_delivery_cost 
+        xml.local_delivery_cost @config.preferred_local_delivery_cost
         xml.vendor product.brand.name if product.brand
         xml.vendorCode product.sku
-        xml.model model_name(product, variant)
+        model = []
+        model << product.brand.name if product.brand.present?
+        model << product.name
+        xml.model model.join(' ')
         xml.description product_description(product) if product_description(product)
         xml.manufacturer_warranty true
         xml.country_of_origin product.country.name if product.country
 
-        xml.variantList do
-          variants.each do |variant|
-            xml.varint(type: "size") do
-              variant.option_values.each do |ov|
-                opt = {system: "RU"}
-                opt[:category] = market_category(product) if market_category(product)
-                opt[:gender] = gender if gender
-                xml.size(ov.presentation, opt)
+        cnt = 0
+        variants.each do |variant|
+          cnt += 1 if variant_size(variant) or variant_color(variant)
+        end
+
+        if cnt > 0
+          xml.variantList do
+            variants.each do |variant|
+              size = variant_size(variant)
+              color = variant_color(variant)
+              if size && color
+                xml_type = "color_and_size"
+              elsif size
+                xml_type = "size"
+              elsif color
+                xml_type = "color"
+              end
+
+              xml.varint(type: xml_type) do
+                variant.option_values.each do |ov|
+                  opt = {}
+                  if ov.option_type.xml_type.include?('size')
+                    opt[:system] =  ov.option_type.xml_system if ov.option_type.try(:xml_system).present?
+                    opt[:category] =  ov.option_type.xml_name if ov.option_type.try(:xml_name).present?
+                    opt[:gender] = gender if gender
+                    xml.size(ov.xml_presentation, opt)
+                  elsif ov.option_type.xml_type.include?('color')
+                    xml.color(ov.xml_presentation, opt)
+                  end
+                end
                 xml.offerId variant.id
               end
             end
           end
         end
 
-
       end
 
+    end
+
+    def variant_size(variant)
+      variant.option_values.find do |ov|
+        ov && ov.option_type.xml_type.include?('size') rescue nil
+      end
+    end
+
+    def variant_color(variant)
+      variant.option_values.find do |ov|
+        ov && ov.option_type.xml_type.include?('color') rescue nil
+      end
     end
 
 
