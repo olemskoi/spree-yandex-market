b03bbc0a0b9a433ff9e083b5a2c132ece44b8110
Artur Trofimov <arturtr@gmail.com>
2014-11-16 16:23:00 +1000

    ym publish only variants with price > 1

--- lib/export/yandex_market_exporter.rb
+++ lib/export/yandex_market_exporter.rb
@@ -86,59 +86,61 @@ module Export
       end
 
       variants.each do |variant|
-        opt = { :type => 'vendor.model', :available => variant.available? }
+        opt = {type: 'vendor.model', available: variant.available?}
 
         opt[:id] = variant.id
         opt[:group_id] = product.id if count > 1
 
         model = model_name(product, variant)
 
-        xml.offer(opt) do
-          xml.url "http://#{@host}/id/#{product.id}#{@utms}"
-          xml.price variant.price
-          xml.oldprice variant.old_price if variant.old_price.to_i > 0
-          xml.currencyId currency_id
-          xml.categoryId product_category_id(product)
-          xml.market_category market_category(product)
-          images.each do |image|
-            xml.picture image_url(image)
-          end
-          xml.delivery true
-          xml.vendor product.brand.name
-          xml.vendorCode product.sku
-          if add_alt_vendor? && product.brand && product.brand.alt_displayed_name.present?
-            xml.vendorAlt product.brand.alt_displayed_name
-          end
-          xml.model model
-          xml.description product_description(product) if product_description(product)
-          xml.sales_notes "Минимальная сумма заказа - #{MIN_ORDER_AMOUNT} руб."
-          xml.country_of_origin product.country.name if product.country
-          xml.barcode variant.barcode if variant.barcode.present?
-          variant.option_values.each do |ov|
-            unless ov.presentation == 'Без размера'
-              unit = product.size_table ? product.size_table.standarted_size_table : 'BRAND'
-              xml.param ov.presentation, :name => ov.option_type.presentation, :unit => unit
+        if variant.price.to_i > 1
+          xml.offer(opt) do
+            xml.url "http://#{@host}/id/#{product.id}#{@utms}"
+            xml.price variant.price
+            xml.oldprice variant.old_price if variant.old_price.to_i > 0
+            xml.currencyId currency_id
+            xml.categoryId product_category_id(product)
+            xml.market_category market_category(product)
+            images.each do |image|
+              xml.picture image_url(image)
             end
+            xml.delivery true
+            xml.vendor product.brand.name
+            xml.vendorCode product.sku
+            if add_alt_vendor? && product.brand && product.brand.alt_displayed_name.present?
+              xml.vendorAlt product.brand.alt_displayed_name
+            end
+            xml.model model
+            xml.description product_description(product) if product_description(product)
+            xml.sales_notes "Минимальная сумма заказа - #{MIN_ORDER_AMOUNT} руб."
+            xml.country_of_origin product.country.name if product.country
+            xml.barcode variant.barcode if variant.barcode.present?
+            variant.option_values.each do |ov|
+              unless ov.presentation == 'Без размера'
+                unit = product.size_table ? product.size_table.standarted_size_table : 'BRAND'
+                xml.param ov.presentation, :name => ov.option_type.presentation, :unit => unit
+              end
+            end
+            xml.param product.colour, :name => 'Цвет'
+            xml.param gender, :name => 'Пол' if gender.present?
+            xml.param product.localized_age, :name => 'Возраст' if product.age
+            xml.param product.picture_type, :name => 'Тип рисунка' if product.picture_type
+            xml.param series(product), name: 'Линейка' if series(product).present?
+            xml.param age_from(variant), name: 'Возраст от', unit: 'месяцев' if age_from(variant).present?
+            xml.param age_to(variant), name: 'Возраст до', unit: 'месяцев' if age_to(variant).present?
+            xml.param variant.width, name: 'Ширина', unit: 'см' if variant.width.present?
+            xml.param variant.height, name: 'Высота', unit: 'см' if variant.height.present?
+            xml.param variant.depth, name: 'Глубина', unit: 'см' if variant.depth.present?
+            xml.param variant.weight, name: 'Вес', unit: 'кг' if variant.weight.present?
+            product.product_properties.each do |product_property|
+              xml.param product_property.value, name: product_property.property.name
+            end
+            if product.orthopedic_properties.present?
+              xml.param product.orthopedic_properties.map(&:name).join(', '), name: 'Ортопедические свойства'
+            end
+            xml.param seasons(product), name: 'Сезоны' if seasons(product).present?
+            additional_params_for_offer(xml, product, variant)
           end
-          xml.param product.colour, :name => 'Цвет'
-          xml.param gender, :name => 'Пол' if gender.present?
-          xml.param product.localized_age, :name => 'Возраст' if product.age
-          xml.param product.picture_type, :name => 'Тип рисунка' if product.picture_type
-          xml.param series(product), name: 'Линейка' if series(product).present?
-          xml.param age_from(variant), name: 'Возраст от', unit: 'месяцев' if age_from(variant).present?
-          xml.param age_to(variant), name: 'Возраст до', unit: 'месяцев' if age_to(variant).present?
-          xml.param variant.width, name: 'Ширина', unit: 'см' if variant.width.present?
-          xml.param variant.height, name: 'Высота', unit: 'см' if variant.height.present?
-          xml.param variant.depth, name: 'Глубина', unit: 'см' if variant.depth.present?
-          xml.param variant.weight, name: 'Вес', unit: 'кг' if variant.weight.present?
-          product.product_properties.each do |product_property|
-            xml.param product_property.value, name: product_property.property.name
-          end
-          if product.orthopedic_properties.present?
-            xml.param product.orthopedic_properties.map(&:name).join(', '), name: 'Ортопедические свойства'
-          end
-          xml.param seasons(product), name: 'Сезоны' if seasons(product).present?
-          additional_params_for_offer(xml, product, variant)
         end
       end
     end
